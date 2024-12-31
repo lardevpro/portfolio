c36a27f4ce6d10a164801d32d8cb5555692af1bd
lardevpro <lardevpro@gmail.com>
2024-12-31 20:19:39 +0100

    drop dow close and body margin-top

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -11,11 +12,24 @@ import { RouterModule } from '@angular/router';
 export class Navbar {
   isMenuOpen = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      document.addEventListener('click', this.handleClickOutside.bind(this));
+    }
+  }
+
   toggleNavbar() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
   closeNavbar() {
     this.isMenuOpen = false;
+  }
+
+  handleClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('app-navbar')) {
+      this.closeNavbar();
+    }
   }
 }
