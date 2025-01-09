3aad258786520e75769c04fa32584a9a7fc6e74c
lardevpro <lardevpro@gmail.com>
2025-01-09 01:00:28 +0100

    install bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,13 @@ import { AboutComponent } from "./about/about.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { ExperienceComponent } from "../experience/experience.component";
 import { TrainingComponent } from "../training/training.component";
+import { CarouselComponent } from "../carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutComponent, ProjectsComponent, ExperienceComponent, TrainingComponent],
+  imports: [AboutComponent, ProjectsComponent, ExperienceComponent, TrainingComponent, CarouselComponent],
   template: ` <app-about></app-about>
+              <app-carousel></app-carousel>
               <app-experience></app-experience>
               <app-training></app-training>
               <app-projects><app-projects>
